d28f105780b365ef1d1de50c7ffed264dfcf5028
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-02-23 16:15:38 +0400

    Setting Kohana framework

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -100,7 +100,7 @@ if (isset($_SERVER['KOHANA_ENV'])) {
  * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-    'base_url' => '/',
+    'base_url' => '/factory-polls/',
     'index_file' => false
 ));
 
