f479f6626311cea9f627fe0e858d4baf9b9dd848
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-02-25 12:20:11 +0400

    Docs updated

--- application/classes/System.php
+++ application/classes/System.php
@@ -2,20 +2,39 @@
 
 defined('SYSPATH') OR die('No direct script access.');
 
+/**
+ * Ядро системы
+ */
 class System {
 
     // Определяет установлена ли система
     private static $_installed = false;
-    // Определяет была ли инициализация или нет (singletone)
+    // Определяет была ли произведена инициализация системы или нет
     private static $_init = false;
 
+    /**
+     * Инициализация системы
+     * @return void
+     */
     public static function ready() {
+        // Инициализация куки
         self::CookieInit();
+        /**
+         * Если система уже проинициализирована нет надобности делать это ещё
+         * раз 
+         */
         if (self::$_init)
             return;
+        /**
+         * Проверяем установлена ли система. Осуществляется путем проверки на 
+         * существовании файла database.php который создается во время установки
+         */
         self::$_installed = file_exists(APPPATH . 'config/database.php');
+        // Если система не установлена
         if (!self::$_installed) {
+            // Сессию будем харнить в куки
             Session::$default = 'cookie';
+            // Устанавливаем роут инсталятора
             Route::set('install', '(install(/<action>))', array(
                         'action' => 'index|systemchek|database|install'
                     ))
@@ -25,6 +44,7 @@ class System {
                     ));
             return;
         }
+        // Система проинициализирована
         self::$_init = true;
     }
 
@@ -32,8 +52,11 @@ class System {
      * Инициализация куки (соль и время жизни)
      */
     private static function CookieInit() {
+        // Если соль куки не установлена, устанавливаем ее из конфигурации
         if (empty(Cookie::$salt)) {
+            // Соль
             Cookie::$salt = Kohana::$config->load('cookie.salt');
+            // Время жизни
             Cookie::$expiration = Kohana::$config->load('cookie.lifetime');
         }
     }
--- application/config/cookie.php
+++ application/config/cookie.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Конфигурация cookie
+ * @var salt string - соль куки
+ * @var lifetime date - время жизни куки
+ */
 return array(
     'salt' => 'we84f5w1e65f',
     'lifetime' => Date::WEEK
