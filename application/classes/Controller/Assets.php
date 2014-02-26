8495c30529b33d8fe45af1db014729cfca78f69e
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-02-27 01:56:33 +0400

    Restruct Asset class

--- application/classes/Controller/Assets.php
+++ application/classes/Controller/Assets.php
@@ -2,92 +2,55 @@
 
 defined('SYSPATH') or die('No direct script access');
 
+/**
+ * Абстрактный класс для инициализиции каталогов содержащих медиа данные.
+ */
 abstract class Controller_Assets extends Controller_Template {
 
-    private $_media;
-    private $_css;
-    private $_systemTheme;
-    private $_plugins;
-    private $_fontAwesome;
-    private $_bootstrap;
-    private $_uniform;
-    private $_select2;
-    private $_img;
-
-    private function setMedia() {
-        $this->_media = File::ChekDir(URL::base() . APPDIR . 'media' . DS);
-        return $this;
-    }
-
-    private function setCss() {
-        $this->_css = File::ChekDir($this->_media . 'css' . DS);
-        return $this;
-    }
-
-    private function setSystemTheme() {
-        $this->_systemTheme = File::ChekDir($this->_css . 'themes' . DS);
-        return $this;
-    }
-
-    private function setPlugins() {
-        $this->_plugins = File::ChekDir($this->_media . 'plugins' . DS);
-        return $this;
-    }
-
-    private function setFontAwesome() {
-        $this->_fontAwesome = File::ChekDir($this->_plugins
-                        . 'font-awesome' . DS . 'css' . DS);
-        return $this;
-    }
-
-    private function setBootstrap() {
-        $this->_bootstrap = File::ChekDir($this->_plugins
-                        . 'bootstrap' . DS . 'css' . DS);
-        return $this;
-    }
-
-    private function setUniform() {
-        $this->_uniform = File::ChekDir($this->_plugins
-                        . 'uniform' . DS . 'css' . DS);
-        return $this;
-    }
-
-    private function setSelect2() {
-        $this->_select2 = File::ChekDir($this->_plugins . 'select2' . DS);
-        return $this;
-    }
-
-    private function setImg() {
-        $this->_img = File::ChekDir($this->_media . 'img' . DS);
-        return $this;
-    }
+    // Корневой массив каталогов для медиа данных
+    private $_assets;
 
     public function before() {
         parent::before();
-        $this->
-                setMedia()
-                ->setCss()
-                ->setSystemTheme()
-                ->setPlugins()
-                ->setFontAwesome()
-                ->setBootstrap()
-                ->setUniform()
-                ->setSelect2()
-                ->setImg();
+        $this->setAssets(Kohana::$config->load('assets'), APPDIR);
+    }
+
+    public function setAssets($config, $directory = '', $level = '') {
+        foreach ($config as $dir => $val) {
+            $this->_assets[$dir] = '';
+            if (is_array($val)) {
+                $level .= '.' . $dir;
+                $directory .= $dir . DS;
+                $this->_assets[$dir] .= $directory;
+                $this->setAssets(
+                        Kohana::$config->load(
+                                'assets' . $level
+                        ), $directory, $level
+                );
+                $this->LevelUp($level, '.');
+                $this->LevelUp($directory, DS);
+            } else {
+                $this->_assets[$dir] = $directory . $val . DS;
+            }
+        }
+    }
+
+    private function LevelUp(&$level, $separator) {
+        $levelUp = explode($separator, $level);
+        if ($separator == DS)
+            array_pop($levelUp);
+        array_pop($levelUp);
+        $level = implode($separator, $levelUp);
+        if ($separator == DS)
+            $level .= DS;
+        unset($levelUp);
+        return $level;
     }
 
     public function getAssets() {
-        return array(
-            'media' => $this->_media,
-            'css' => $this->_css,
-            'system_theme' => $this->_systemTheme,
-            'plugins' => $this->_plugins,
-            'font_awesome' => $this->_fontAwesome,
-            'bootstrap' => $this->_bootstrap,
-            'uniform' => $this->_uniform,
-            'select2' => $this->_select2,
-            'img' => $this->_img
-        );
+        foreach ($this->_assets as $asset)
+            File::ChekDir($asset, DOCROOT);
+        return $this->_assets;
     }
 
 }
---
+++ application/config/assets.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('SYSPATH') or die('No direct script access');
+
+return array(
+    'media' => array(
+        'css' => array(
+            'themes_css' => 'themes',
+        ),
+        'plugins' => array(
+            'font-awesome' => array(
+                'font-awesome_css' => 'css'
+            ),
+            'bootstrap' => array(
+                'bootstrap_css' => 'css'
+            ),
+            'uniform' => array(
+                'uniform_css' => 'css'
+            ),
+            'select2' => 'select2'
+        ),
+        'img' => 'img'
+    )
+);
+?>
