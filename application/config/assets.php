2714195797caa471b4eb1b7da1840a2a0c355bc9
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-02-27 12:16:31 +0400

    Config assets updated

--- application/config/assets.php
+++ application/config/assets.php
@@ -13,13 +13,21 @@ return array(
                     'font-awesome_css' => 'css'
                 ),
                 'bootstrap' => array(
-                    'bootstrap_css' => 'css'
+                    'bootstrap_css' => 'css',
+                    'bootstrap_js' => 'js',
                 ),
                 'uniform' => array(
                     'uniform_css' => 'css'
                 ),
-                'select2' => 'select2'
+                'select2' => 'select2',
+                'bootstrap-hover-dropdown' => 'bootstrap-hover-dropdown',
+                'jquery-slimscroll' => 'jquery-slimscroll',
+                'jquery-validation' => array(
+                    'jquery-validation_dist' => 'dist'
+                ),
+                'bootstrap-wizard' => 'bootstrap-wizard'
             ),
+            'scripts' => 'scripts',
             'img' => 'img'
         )
     )
