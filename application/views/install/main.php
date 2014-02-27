7086b1b5607150efb90dd568dd6764fb4b50fac6
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-02-27 13:30:01 +0400

    Install main template

--- application/views/install/main.php
+++ application/views/install/main.php
@@ -1,10 +1,123 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!--> <html lang="ru" class="no-js"> <!--<![endif]-->
     <head>
-        <meta charset="utf8">
-        <title>Установка</title>
+        <base href="<?php echo URL::base(TRUE) ?>">
+        <meta charset="utf-8" />
+        <title>Factory Polls | Установка</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta content="" name="description" />
+        <meta content="" name="author" />
+        <meta name="MobileOptimized" content="320">
+        <link href="<?php echo $assets['font-awesome_css']; ?>font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['bootstrap_css']; ?>bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['uniform_css']; ?>uniform.default.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['select2']; ?>select2_metro.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo $assets['css']; ?>style-metronic.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['css']; ?>style.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['css']; ?>style-responsive.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['css']; ?>plugins.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo $assets['themes_css']; ?>default.css" rel="stylesheet" type="text/css" id="style_color"/>
+        <link href="<?php echo $assets['css']; ?>custom.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="favicon.ico" />
     </head>
-    <body>
-        Крутяк!
+    <body class="page-header-fixed">
+        <div class="header navbar navbar-inverse navbar-fixed-top">
+            <div class="header-inner">
+                <a class="navbar-brand" href="<?php echo URL::base(); ?>">
+                    <img src="<?php echo $assets['img']; ?>logo.png" alt="logo" class="img-responsive" />
+                </a>
+                <a href="javascript:;" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <img src="<?php echo $assets['img']; ?>menu-toggler.png" alt="" />
+                </a> 
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="page-container">
+            <div class="page-sidebar navbar-collapse collapse">
+                <ul class="page-sidebar-menu">
+                    <li>
+                        <div class="sidebar-toggler hidden-phone"></div>
+                    </li>
+                    <li class="start active">
+                        <a href="javascript:;">
+                            <i class="fa fa-cogs"></i>
+                            <span class="title">Установка</span>
+                            <span class="selected "></span>
+                            <ul class="sub-menu">
+                                <li>
+                                    <a href="#">
+                                        Учетная запись
+                                    </a>
+                                </li>
+                                <li class="disabled-link">
+                                    <a href="#">
+                                        <span class="text">Проверка системы</span></a>
+                                </li>
+                                <li class="disabled-link">
+                                    <a href="#">
+                                        <span class="text">Настройки базы данных</span></a>
+                                </li>
+                                <li class="disabled-link">
+                                    <a href="#">
+                                        <span class="text">Настройки проекта</span></a>
+                                </li>
+                            </ul>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="page-content">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="page-title">
+                            Установка системы <small>первичная установка системы</small>
+                        </h3>
+                        <ul class="page-breadcrumb breadcrumb">
+                            <li>
+                                <i class="fa fa-cogs"></i>
+                                <a href="<?php echo URL::base(); ?>">Установка</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <?php echo $content;?>
+            </div>
+        </div>
+        <div class = "footer">
+            <div class = "footer-inner">
+                2014 &copy;
+                Alexey Yakovlev.
+            </div>
+            <div class = "footer-tools">
+                <span class = "go-top">
+                    <i class = "fa fa-angle-up"></i>
+                </span>
+            </div>
+        </div>
+        <!--[if lt IE 9]>
+        <script src = "<?php echo $assets['plugins']; ?>respond.min.js"></script>
+                <script src="<?php echo $assets['plugins']; ?>excanvas.min.js"></script> 
+            <![endif]-->   
+        <script src="<?php echo $assets['plugins']; ?>jquery-1.10.2.min.js" type="text/javascript"></script>
+        <script src="<?php echo $assets['plugins']; ?>jquery-migrate-1.2.1.min.js" type="text/javascript"></script>    
+        <script src="<?php echo $assets['bootstrap_js']; ?>bootstrap.min.js" type="text/javascript"></script>
+        <script src="<?php echo $assets['bootstrap-hover-dropdown']; ?>twitter-bootstrap-hover-dropdown.min.js" type="text/javascript" ></script>
+        <script src="<?php echo $assets['jquery-slimscroll']; ?>jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="<?php echo $assets['plugins']; ?>jquery.blockui.min.js" type="text/javascript"></script>  
+        <script src="<?php echo $assets['plugins']; ?>jquery.cookie.min.js" type="text/javascript"></script>
+        <script src="<?php echo $assets['uniform']; ?>jquery.uniform.min.js" type="text/javascript" ></script>
+        <script type="text/javascript" src="<?php echo $assets['jquery-validation_dist']; ?>jquery.validate.min.js"></script>
+        <script type="text/javascript" src="<?php echo $assets['jquery-validation_dist']; ?>additional-methods.min.js"></script>
+        <script type="text/javascript" src="<?php echo $assets['bootstrap-wizard']; ?>jquery.bootstrap.wizard.min.js"></script>
+        <script type="text/javascript" src="<?php echo $assets['select2']; ?>select2.min.js"></script>
+        <script src="<?php echo $assets['scripts']; ?>app.js"></script>
+        <script>
+            $(document).ready(function() {       
+                App.init();
+            });
+        </script>
     </body>
 </html>
