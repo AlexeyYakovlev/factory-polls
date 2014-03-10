757cf415080a13b1dc5a6dba285a26d7ef6ada49
AlexeyYakovlev <yakovlev@radar-research.ru>
2014-03-10 21:36:24 +0400

    Динамический заголовок прогрессбара

--- application/views/install/stepdesc.php
+++ application/views/install/stepdesc.php
@@ -1,7 +1,7 @@
 <div class="portlet-title">
     <div class="caption">
         <i class="fa fa-reorder"></i> 
-        Учтеная запись администратора - 
+        <?php echo $g_menuitems[Request::current()->action()]['text']; ?> - 
         <span class="step-title">
             шаг <?php echo $g_menuitems[Request::current()->action()]['step']; ?> из <?php echo count($g_menuitems); ?>
         </span>
